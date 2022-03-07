4bff031ea5f8adf6f0b139aa3307c651e109a56c
Chris Sim <monkeymon@gmail.com>
2022-03-07 21:32:48 +0800

    adding new secrets

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ async function run() {
     const imageURI = core.getInput('image', { required: true });
 
     const environmentVariables = core.getInput('environment-variables', { required: false });
+    const secretVariables = core.getInput('secret-variables', { required: false });
 
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
@@ -66,6 +67,38 @@ async function run() {
         } else {
           // Else, create
           containerDef.environment.push(variable);
+        }
+      })
+    }
+
+    if (secretVariables) {
+      containerDef.secrets = [];
+      // Get pairs by splitting on newlines
+      secretVariables.split('\n').forEach(function (line) {
+        // Trim whitespace
+        const trimmedLine = line.trim();
+        // Skip if empty
+        if (trimmedLine.length === 0) { return; }
+        // Split on =
+        const separatorIdx = trimmedLine.indexOf("=");
+        // If there's nowhere to split
+        if (separatorIdx === -1) {
+            throw new Error(`Cannot parse the secret variable '${trimmedLine}'. Environment variable pairs must be of the form NAME=value.`);
+        }
+        // Build object
+        const variable = {
+          name: trimmedLine.substring(0, separatorIdx),
+          valueFrom: trimmedLine.substring(separatorIdx + 1),
+        };
+
+        // Search container definition environment for one matching name
+        const variableDef = containerDef.secrets.find((e) => e.name == variable.name);
+        if (variableDef) {
+          // If found, update
+          variableDef.value = variable.value;
+        } else {
+          // Else, create
+          containerDef.secrets.push(variable);
         }
       })
     }
